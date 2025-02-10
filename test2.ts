08331699b67d9c4def2eec2d92c7f812fb5e8826
mustafinDamir2023 <mustafind5@gmail.com>
2025-02-10 20:30:47 +0300

    solo commit

---
+++ test2.ts
@@ -0,0 +1,7 @@
+import { prepareString } from './test1'
+
+const names1: string[] = ['john', 'paul']
+const names2: string[] = ['george', 'ringo']
+const names3: string[] = ['lennon', 'mick']
+
+console.log(prepareString(names1))
