cc37327aebae48a86bc53bb33d556dbdadf04a0e
mustafinDamir2023 <mustafind5@gmail.com>
2025-02-11 14:02:19 +0300

    edit test2

--- test2.ts
+++ test2.ts
@@ -2,5 +2,7 @@ import { prepareString } from './test1'
 
 const names0: string[] = ['larry', 'moe', 'curly']
 
+const names1: string[] = ['van', 'daan', 'jeroen', 'sander']
+const names2: string[] = ['john', 'paul', 'george', 'ringo']
 
 console.log(prepareString(names1))
