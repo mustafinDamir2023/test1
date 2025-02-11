c87338780710d146b50289a6b1bb107fdb51354f
mustafinDamir2023 <mustafind5@gmail.com>
2025-02-11 13:41:02 +0300

    add sew names

--- test2.ts
+++ test2.ts
@@ -1,5 +1,7 @@
 import { prepareString } from './test1'
 
-const names: string[] = ['john', 'paul', 'george', 'ringo']
+const names0: string[] = ['larry', 'moe', 'curly']
+const names1: string[] = ['van', 'daan', 'jeroen', 'sander']
+const names2: string[] = ['john', 'paul', 'george', 'ringo']
 
-console.log(prepareString(names))
+console.log(prepareString(names1))
