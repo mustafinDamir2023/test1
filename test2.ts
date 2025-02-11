df5a8c4f5c5dbc7695f74f868595c9adf0e4b9ed
mustafinDamir2023 <mustafind5@gmail.com>
2025-02-11 14:06:25 +0300

    delete names

--- test2.ts
+++ test2.ts
@@ -1,7 +1,6 @@
 import { prepareString } from './test1'
 
 const names0: string[] = ['larry', 'moe', 'curly']
-const names1: string[] = ['van', 'daan', 'jeroen', 'sander']
-const names2: string[] = ['john', 'paul', 'george', 'ringo']
+
 
 console.log(prepareString(names1))
