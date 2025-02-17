c811044788cf9f2393f9fbee07d16235954556f3
mustafinDamir2023 <mustafind5@gmail.com>
2025-02-17 12:31:59 +0300

    mq2

--- test2.ts
+++ test2.ts
@@ -4,5 +4,7 @@ const names1: string[] = ['john', 'paul']
 const names2: string[] = ['george', 'ringo']
 const names3: string[] = ['lennon', 'mick']
 const names4: string[] = ['main', 'main']
+const names5: string[] = ['main', 'main', 'main']
 
 console.log(prepareString(names1))
+console.log(prepareString(names2))
