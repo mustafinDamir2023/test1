f8a51fab4526603430ca943485c8e42bfbfb5a49
mustafinDamir2023 <mustafind5@gmail.com>
2025-02-17 12:31:04 +0300

    mq1

--- test2.ts
+++ test2.ts
@@ -3,5 +3,6 @@ import { prepareString } from './test1'
 const names1: string[] = ['john', 'paul']
 const names2: string[] = ['george', 'ringo']
 const names3: string[] = ['lennon', 'mick']
+const names4: string[] = ['main', 'main']
 
 console.log(prepareString(names1))
