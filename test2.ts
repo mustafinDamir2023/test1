d989d27bd5a2dc0daa2eb4a6b393ec56bcee2d82
mustafinDamir2023 <mustafind5@gmail.com>
2025-02-17 11:47:19 +0300

    commonQommit

--- test2.ts
+++ test2.ts
@@ -3,8 +3,10 @@ import { prepareString } from './test1'
 const names1: string[] = ['john', 'paul']
 const names2: string[] = ['george', 'ringo']
 const names3: string[] = ['lennon', 'mick']
-const names4: string[] = ['main', 'main']
-const names5: string[] = ['main', 'main', 'main']
+const names4: string[] = ['john', 'ringo']
+const names5: string[] = ['paul', 'mick']
+const names6: string[] = ['main', 'main']
+const names7: string[] = ['main', 'main', 'main']
 
 console.log(prepareString(names1))
 console.log(prepareString(names2))
