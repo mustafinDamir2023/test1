cc61650af92c9896e282ca8c5338035398c957c5
mustafinDamir2023 <mustafind5@gmail.com>
2025-02-23 17:43:57 +0300

    edit commit

--- test2.ts
+++ test2.ts
@@ -2,5 +2,10 @@ import { prepareString } from './test1'
 
 const names1: string[] = ['john', 'paul']
 const names2: string[] = ['george', 'ringo']
+const names3: string[] = ['john', 'paul']
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
 
 console.log(prepareString(names1))
